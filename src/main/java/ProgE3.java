e26de23330025a5ce09f8e255796c4fad712d47e
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-06-07 17:11:16 +0900

    Update ProgE3.java

--- src/main/java/ProgE3.java
+++ src/main/java/ProgE3.java
@@ -5,11 +5,11 @@ import java.io.IOException;
 
 public class ProgE3 {
 
-	public static void main(String[] args) {
-		String fname = args[0];
-		String[] locations = {"中央区", "北区(太平)", "北区(あいの里)", "東区", "白石区", "厚別区", "豊平区", "清田区", "南区(南31条西8丁目)", "南区(定山渓)", "西区(西野)", "西区(平和)","手稲区"};
+    public static void main(String[] args) {
+        String fname = args[0];
+        String[] locations = {"中央区", "北区(太平)", "北区(あいの里)", "東区", "白石区", "厚別区", "豊平区", "清田区", "南区(南31条西8丁目)", "南区(定山渓)", "西区(西野)", "西区(平和)","手稲区"};
 
-	}
+    }
 
 }
 
